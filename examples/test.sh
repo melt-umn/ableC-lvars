a853376e185e56d5a72b2c9aa7b7add24f4bcf35
Ciaradh Coomey <comme043@umn.edu>
2018-08-06 16:09:36 -0500

    test.sh fixed w/ nproc placement

--- examples/test.sh
+++ examples/test.sh
@@ -1,27 +1,21 @@
-time ./bitFold.out 10000 25 1 -nproc 1
-time ./bitFold.out 10000 25 1 -nproc 2
-time ./bitFold.out 10000 25 1 -nproc 3
-time ./bitFold.out 10000 25 1 -nproc 4
-time ./bitFold.out 10000 25 1 -nproc 5
-time ./bitFold.out 10000 25 1 -nproc 6
-time ./bitFold.out 10000 25 1 -nproc 7
-time ./bitFold.out 10000 25 1 -nproc 8
-time ./bitFold.out 10000 25 1 -nproc 9
-time ./bitFold.out 10000 25 1 -nproc 10
-time ./bitFold.out 10000 25 1 -nproc 11
-time ./bitFold.out 10000 25 1 -nproc 12
-time ./bitFold.out 10000 25 1 -nproc 13
-time ./bitFold.out 10000 25 1 -nproc 14
-time ./bitFold.out 10000 25 1 -nproc 15
-time ./bitFold.out 10000 25 1 -nproc 16
-time ./bitFold.out 10000 25 1 -nproc 17
-time ./bitFold.out 10000 25 1 -nproc 18
-time ./bitFold.out 10000 25 1 -nproc 19
-time ./bitFold.out 10000 25 1 -nproc 20
-time ./bitFold.out 10000 25 1 -nproc 21
-time ./bitFold.out 10000 25 1 -nproc 22
-time ./bitFold.out 10000 25 1 -nproc 23
-time ./bitFold.out 10000 25 1 -nproc 24
-
-
+echo '1 chunk'
+time ./bitFold.out -nproc 1 10000 25 1 
+time ./bitFold.out -nproc 2 10000 25 1 
+time ./bitFold.out -nproc 3 10000 25 1
+time ./bitFold.out -nproc 4 10000 25 1
+echo '2 chunks'
+time ./bitFold.out -nproc 1 10000 25 2
+time ./bitFold.out -nproc 2 10000 25 2 
+time ./bitFold.out -nproc 3 10000 25 2
+time ./bitFold.out -nproc 4 10000 25 2
+echo '3 chunks'
+time ./bitFold.out -nproc 1 10000 25 3
+time ./bitFold.out -nproc 2 10000 25 3 
+time ./bitFold.out -nproc 3 10000 25 3
+time ./bitFold.out -nproc 4 10000 25 3
+echo '4 chunks'
+time ./bitFold.out -nproc 1 10000 25 4
+time ./bitFold.out -nproc 2 10000 25 4 
+time ./bitFold.out -nproc 3 10000 25 4
+time ./bitFold.out -nproc 4 10000 25 4
 
